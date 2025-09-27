cc1fa8404b20fc5e6c2c128b669d258188d45ed5
Michael <michaelgiu2012@outlook.com>
2025-09-27 14:23:34 -0400

    Added background colors

--- color.h
+++ color.h
@@ -5,8 +5,8 @@
 #ifndef CPU_TEST_COLOR_H
 #define CPU_TEST_COLOR_H
 
-//the following are UBUNTU/LINUX, and macOS ONLY terminal color codes.
-#define RESET   "\033[0m"
+// Foreground Colors
+#define RESET   "\033[0m"       /* Reset All Colors*/
 #define BLACK   "\033[30m"      /* Black */
 #define RED     "\033[31m"      /* Red */
 #define GREEN   "\033[32m"      /* Green */
@@ -15,13 +15,33 @@
 #define MAGENTA "\033[35m"      /* Magenta */
 #define CYAN    "\033[36m"      /* Cyan */
 #define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
-#define BOLDBACKGREEN  "\033[1m\033[42m" /* Bold Green Background */
-#endif //CPU_TEST_COLOR_He
+
+// Bold Foreground Colors
+#define BOLD_BLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLD_RED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLD_GREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLD_YELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLD_BLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLD_MAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLD_CYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLD_WHITE   "\033[1m\033[37m"      /* Bold White */
+
+// Background Colors
+#define BACK_RED     "\033[41m" /* Red Background */
+#define BACK_GREEN   "\033[42m" /* Green Background */
+#define BACK_YELLOW  "\033[43m" /* Yellow Background */
+#define BACK_BLUE    "\033[44m" /* Blue Background */
+#define BACK_MAGENTA "\033[45m" /* Magenta Background */
+#define BACK_CYAN    "\033[46m" /* Cyan Background */
+#define BACK_WHITE   "\033[47m" /* White Background */
+
+// Bold Background Colors
+#define BOLD_BACK_RED     "\033[1m\033[41m" /* Bold Red Background */
+#define BOLD_BACK_GREEN   "\033[1m\033[42m" /* Bold Green Background */
+#define BOLD_BACK_YELLOW  "\033[1m\033[43m" /* Bold Yellow Background */
+#define BOLD_BACK_BLUE    "\033[1m\033[44m" /* Bold Blue Background */
+#define BOLD_BACK_MAGENTA "\033[1m\033[45m" /* Bold Magenta Background */
+#define BOLD_BACK_CYAN    "\033[1m\033[46m" /* Bold Cyan Background */
+#define BOLD_BACK_WHITE   "\033[1m\033[47m" /* Bold White Background */
+
+#endif //CPU_TEST_COLOR_H
