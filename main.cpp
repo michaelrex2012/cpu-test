c7e84655250c2f2c82e0580148da46387d873675
Michael G. <michaelgiu2012@outlook.com>
2025-09-30 15:59:02 -0400

    added some more flags

--- main.cpp
+++ main.cpp
@@ -18,14 +18,15 @@ int main(int argc, char* argv[]) {
     int priority = 3;
     string versionNum = "v0.1 Beta";
     string mode = "run";
+    string outputPath = "output.log";
 
     for (int i = 1; i < argc; i++) {
         string arg = argv[i];
 
-        if (arg == "-p" || arg == "--cpu-priority" && i + 1 < argc) {
+        if ((arg == "-p" || arg == "--cpu-priority") && i + 1 < argc) {
             priority = stoi(argv[++i]);
         }
-        if (arg == "-t" || arg == "--time" && i + 1 < argc) {
+        if ((arg == "-t" || arg == "--time") && i + 1 < argc) {
             runTime = stoi(argv[++i]);
         }
         if (arg == "-h" || arg == "--help") {
@@ -36,6 +37,12 @@ int main(int argc, char* argv[]) {
         }
         if (arg == "-i" || arg == "--info") {
             mode = "info";
+        }
+        if ((arg == "-o" || arg == "--output") && i + 1 < argc) {
+            outputPath = argv[++i];
+        }
+        if (arg == "-no-ansi") {
+            mode = "no-ansi";
         }
     }
 
@@ -64,7 +71,7 @@ int main(int argc, char* argv[]) {
     }
 
     if (mode == "info") {
-        ofstream infoFile("info.log");
+        ofstream infoFile(outputPath);
 
         cout << "[" << chrono::system_clock::now() << "] "
                      << BACK_YELLOW << "INFO" << RESET << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
@@ -82,10 +89,62 @@ int main(int argc, char* argv[]) {
         cout << "-p --cpu-priority: Set run priority (Default unset, 1 is low, 3 is normal, and 5 is realtime)" << endl;
         cout << "-i --info: Displays machine info and exits" << endl;
         cout << "-t --time: Set time to run" << endl;
+        cout << "-o --output: Specify an output file" << endl;
+        cout << "--no-ansi: Disables ANSI color codes" << endl;
+        cout << "-s --silent: Disables output to stout but still outputs to log file" << endl;
+        cout << "--no-pause: Disables pause at end of benchmark";
     }
 
     if (mode == "run") {
-        ofstream logFile("output.log");
+        ofstream logFile(outputPath);
+
+        int counter = 0;
+        int beforeTime = -1;
+        int iterations = 0;
+        int totalTime = 0;
+        cout << "[" << chrono::system_clock::now() << "] "
+                     << BACK_YELLOW << "INFO" << RESET << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+                     << "INFO" << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
+        cout << "[" << chrono::system_clock::now() << "] "
+                     << BACK_YELLOW << "INFO" << RESET << " " << "Number of Cores: " << sysInfo.dwNumberOfProcessors << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+                     << "INFO" << " " << "Number of Cores: " << sysInfo.dwNumberOfProcessors << endl;
+        cout << "[" << chrono::system_clock::now() << "] "
+                     << BACK_YELLOW << "INFO" << RESET << " " << "Priority of this test: " << priority << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+                     << "INFO" << " " << "Priority of this test: " << priority << endl;
+
+        const auto start = chrono::system_clock::now();
+
+        while (totalTime < runTime) {
+            iterations++;
+            auto end = chrono::system_clock::now();
+            totalTime = chrono::duration_cast<chrono::seconds>(end - start).count();
+
+            if (totalTime != beforeTime)
+            {
+                cout << "[" << chrono::system_clock::now() << "] "
+                     << BOLD_BACK_GREEN << "PROGRESS" << RESET << " " << totalTime << "/" << runTime << " s" << endl;
+                logFile << "[" << chrono::system_clock::now() << "] "
+                     << "PROGRESS" << " " << totalTime << "/" << runTime << " s" << endl;
+                beforeTime = totalTime;
+                counter++;
+            }
+        }
+
+        cout << "[" << chrono::system_clock::now() << "] "
+             << BOLD_BACK_MAGENTA << "RESULT" << RESET << " "
+             << (iterations / runTime) / 1000 << " ln/ms" << endl << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+             << "RESULT" << " "
+             << (iterations / runTime) / 1000 << " ln/ms";
+
+        system("pause");
+    }
+
+    if (mode == "no-ansi") {
+        ofstream logFile(outputPath);
 
         int counter = 0;
         int beforeTime = -1;
