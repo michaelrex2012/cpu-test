41b4bcb5c5409252af417aeff7da3d5b63f83a25
Michael G. <michaelgiu2012@outlook.com>
2025-09-30 13:34:10 -0400

    Added all command flags

--- main.cpp
+++ main.cpp
@@ -17,11 +17,9 @@ int main(int argc, char* argv[]) {
     ofstream logFile("output.log");
 
     int runTime = 60;
-    int iterations = 0;
-    int totalTime = 0;
-    int beforeTime = -1;
-    int counter = 0;
     int priority = 3;
+    string versionNum = "v0.1 Beta";
+    string mode = "run";
 
     for (int i = 1; i < argc; i++) {
         string arg = argv[i];
@@ -32,43 +30,93 @@ int main(int argc, char* argv[]) {
         if (arg == "-t" || arg == "--time" && i + 1 < argc) {
             runTime = stoi(argv[++i]);
         }
-    }
-
-
-    cout << "[" << chrono::system_clock::now() << "] "
-                 << BACK_YELLOW << "INFO" << RESET << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
-    logFile << "[" << chrono::system_clock::now() << "] "
-                 << "INFO" << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
-    cout << "[" << chrono::system_clock::now() << "] "
-                 << BACK_YELLOW << "INFO" << RESET << " " << "Number of Cores: " << sysInfo.dwNumberOfProcessors << endl;
-    logFile << "[" << chrono::system_clock::now() << "] "
-                 << "INFO" << " " << "Number of Cores: " << sysInfo.dwNumberOfProcessors << endl;
-
-    const auto start = chrono::system_clock::now();
-
-    while (totalTime < runTime) {
-        iterations++;
-        auto end = chrono::system_clock::now();
-        totalTime = chrono::duration_cast<chrono::seconds>(end - start).count();
-
-        if (totalTime != beforeTime)
-        {
-            cout << "[" << chrono::system_clock::now() << "] "
-                 << BOLD_BACK_GREEN << "PROGRESS" << RESET << " " << totalTime << "/" << runTime << " s" << endl;
-            logFile << "[" << chrono::system_clock::now() << "] "
-                 << "PROGRESS" << " " << totalTime << "/" << runTime << " s" << endl;
-            beforeTime = totalTime;
-            counter++;
+        if (arg == "-h" || arg == "--help") {
+            mode = "help";
+        }
+        if (arg == "-v" || arg == "--version") {
+            mode = "version";
+        }
+        if (arg == "-i" || arg == "--info") {
+            mode = "info";
         }
     }
 
-    cout << "[" << chrono::system_clock::now() << "] "
-         << BOLD_BACK_MAGENTA << "RESULT" << RESET << " "
-         << (iterations / runTime) / 1000 << " ln/ms" << endl << endl;
-    logFile << "[" << chrono::system_clock::now() << "] "
-         << "RESULT" << " "
-         << (iterations / runTime) / 1000 << " ln/ms";
+    if (priority == 1) {
+        SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+    }
+    if (priority == 2) {
+        SetPriorityClass(GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS);
+    }
+    if (priority == 3) {
+        SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+    }
+    if (priority == 4) {
+        SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS);
+    }
+    if (priority == 5) {
+        SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
+    }
 
-    system("pause");
+    if (priority < 1 || priority > 5) {
+        priority = 3;
+    }
+
+    if (mode == "info") {
+        cout << versionNum;
+    }
+
+    if (mode == "help") {
+        cout << "-h --help: Displays this help and exits" << endl;
+        cout << "-v --version: Displays this program's version number and exits" << endl;
+        cout << "-p --cpu-priority: Set run priority (Default unset, 1 is low, 3 is normal, and 5 is realtime)" << endl;
+        cout << "-i --info: Displays machine info and exits" << endl;
+        cout << "-t --time: Set time to run" << endl;
+    }
+
+    if (mode == "run") {
+        int counter = 0;
+        int beforeTime = -1;
+        int iterations = 0;
+        int totalTime = 0;
+        cout << "[" << chrono::system_clock::now() << "] "
+                     << BACK_YELLOW << "INFO" << RESET << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+                     << "INFO" << " " <<"Page Size: " << sysInfo.dwPageSize << " B" << endl;
+        cout << "[" << chrono::system_clock::now() << "] "
+                     << BACK_YELLOW << "INFO" << RESET << " " << "Number of Cores: " << sysInfo.dwNumberOfProcessors << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+                     << "INFO" << " " << "Number of Cores: " << sysInfo.dwNumberOfProcessors << endl;
+        cout << "[" << chrono::system_clock::now() << "] "
+                     << BACK_YELLOW << "INFO" << RESET << " " << "Priority of this test: " << priority << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+                     << "INFO" << " " << "Priority of this test: " << priority << endl;
+
+        const auto start = chrono::system_clock::now();
+
+        while (totalTime < runTime) {
+            iterations++;
+            auto end = chrono::system_clock::now();
+            totalTime = chrono::duration_cast<chrono::seconds>(end - start).count();
+
+            if (totalTime != beforeTime)
+            {
+                cout << "[" << chrono::system_clock::now() << "] "
+                     << BOLD_BACK_GREEN << "PROGRESS" << RESET << " " << totalTime << "/" << runTime << " s" << endl;
+                logFile << "[" << chrono::system_clock::now() << "] "
+                     << "PROGRESS" << " " << totalTime << "/" << runTime << " s" << endl;
+                beforeTime = totalTime;
+                counter++;
+            }
+        }
+
+        cout << "[" << chrono::system_clock::now() << "] "
+             << BOLD_BACK_MAGENTA << "RESULT" << RESET << " "
+             << (iterations / runTime) / 1000 << " ln/ms" << endl << endl;
+        logFile << "[" << chrono::system_clock::now() << "] "
+             << "RESULT" << " "
+             << (iterations / runTime) / 1000 << " ln/ms";
+
+        system("pause");
+    }
     return 0;
 }
